d0fefa2e5b6b24e07ffdbadb3ae4da61c727fe56
leylashd <leylashd99@gmail.com>
2024-06-06 17:40:35 +0700

    style : improve UI login

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,7 +1,16 @@
+import { LoginHero } from "@/components/auth/LoginHero";
+
 export default function Layout({ children }) {
   return (
     <main className="h-screen flex justify-center items-center">
-      <div className="w-[350px]">{children}</div>
+      <div className="grid grid-cols-2 h-[500px] rounded-3xl shadow-2xl shadow-black">
+        <div className="w-[500px] h-auto bg-base-content rounded-l-3xl ">
+          <LoginHero />
+        </div>
+        <div className="w-[500px] bg-base-300 flex justify-center items-center rounded-r-3xl">
+          <div className="w-[350px]">{children}</div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/auth/login.jsx
+++ src/components/auth/login.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import toast from "react-hot-toast";
@@ -27,46 +28,84 @@ export const Login = () => {
       router.push("/dashboard");
       router.refresh();
     } else {
+      toast.error("Login failed");
       setMessage(data.message);
     }
   }
 
   return (
     <main className="space-y-6">
-      <section className="flex justify-center items-center w-full">
-        <h1 className="text-2xl">Welcome back</h1>
+      <section className="items-center w-full">
+        <h1 className="text-3xl flex justify-center font-bold font-sans ">
+          Welcome back
+        </h1>
+        <p className="flex justify-center text-gray-600 pt-2">
+          Please login to your account
+        </p>
       </section>
       <form action={handleLogin} disabled={loading} className="space-y-2">
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          className="input input-bordered input-secondary w-full"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className="input input-bordered input-secondary w-full"
-        />
-        <button className="btn btn-secondary w-full my-3 disabled:opacity-40 disabled:cursor-wait">
+        <label className="input input-bordered flex items-center gap-2 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="email"
+            name="email"
+            className="grow"
+            placeholder="Email"
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            className="grow"
+            name="password"
+            placeholder="Password"
+          />
+        </label>
+        <button className="btn w-full my-3 disabled:opacity-40 disabled:cursor-wait">
           Sign in
         </button>
+        <p className="flex justify-center pt-1 font-sans text-gray-600">
+          Don't have an account?{" "}
+          <Link href="/register" className="pl-1 text-blue-600">
+            {" "}
+            Sign Up
+          </Link>
+        </p>
       </form>
       {message !== "" ? (
         <div role="alert" className="alert alert-warning">
           <svg
             xmlns="http://www.w3.org/2000/svg"
+            className="stroke-current shrink-0 h-6 w-6"
             fill="none"
             viewBox="0 0 24 24"
-            className="stroke-current shrink-0 w-6 h-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            ></path>
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+            />
           </svg>
           <span>{message}</span>
         </div>
