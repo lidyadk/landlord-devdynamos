bbc7ab472224a8f5e68c85d08f832514af719cc8
leylashd <leylashd99@gmail.com>
2024-06-06 21:56:52 +0700

    style : improve UI register + add toaster

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const Register = () => {
+  const router = useRouter();
   const formRef = useRef();
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
@@ -29,66 +33,121 @@ export const Register = () => {
     console.log({ username, email, password, role });
 
     const data = await response.json();
-    setMessage(data.message);
-    formRef.current.reset();
+
+    if (response.status === 201) {
+      toast.success("User registered success");
+      setEmail("");
+      setUsername("");
+      setPassword("");
+      router.push("/login");
+    } else {
+      setMessage(data.message);
+      toast.error("All field must be filled");
+    }
   };
 
   return (
     <div className="space-y-6">
-      <section className="flex justify-center items-center w-full">
-        <h1 className="text-2xl">Create an account</h1>
+      <section className="items-center w-full">
+        <h1 className="text-3xl flex justify-center font-bold font-sans ">
+          Create an account
+        </h1>
+        <p className="flex justify-center text-gray-600 pt-2">
+          Please fill this form to register
+        </p>
       </section>
       <form ref={formRef} onSubmit={handleSubmit} className="space-y-2">
-        <input
-          type="text"
-          value={username}
-          placeholder="Name"
-          onChange={(e) => setUsername(e.target.value)}
-          className="input input-bordered input-secondary w-full"
-        />
-        <input
-          type="email"
-          value={email}
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="input input-bordered input-secondary w-full"
-        />
-        <input
-          type="password"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-          className="input input-bordered input-secondary w-full"
-        />
+        <label className="input input-bordered flex items-center gap-2 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            placeholder="Name"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+            <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+          </svg>
+          <input
+            type="text"
+            className="grow"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label className="input input-bordered flex items-center gap-2 w-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="w-4 h-4 opacity-70"
+          >
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            className="grow"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
         <div className="flex py-2">
           <label className="flex">
             <input
               type="radio"
               value="LANDLORD"
-              name="radio-5"
-              className="radio radio-secondary mx-2"
+              name="radio-1"
+              className="radio mx-2"
               checked={role === "LANDLORD"}
               onChange={handleRoleChange}
             />
-            <p>LANDLORD</p>
+            <p className="font-sans">LANDLORD</p>
           </label>
           <label className="flex">
             <input
               type="radio"
               value="TENANT"
-              name="radio-5"
-              className="radio radio-secondary mx-2"
+              name="radio-1"
+              className="radio mx-2"
               checked={role === "TENANT"}
               onChange={handleRoleChange}
             />
-            <p>TENANT</p>
+            <p className="font-sans">TENANT</p>
           </label>
         </div>
-        <button type="submit" className="btn btn-secondary w-full my-3">
+        <button type="submit" className="btn w-full my-3">
           Sign Up
         </button>
+        <p className="flex justify-center pt-1 font-sans text-gray-600">
+          Already have an account?
+          <Link href="/login" className="pl-1 text-blue-600">
+            Sign In
+          </Link>
+        </p>
       </form>
-      {message !== "" ? (
+      {/* {message !== "" ? (
         <div role="alert" className="alert alert-warning">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -100,12 +159,12 @@ export const Register = () => {
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
             ></path>
           </svg>
           <span>{message}</span>
         </div>
-      ) : null}
+      ) : null} */}
     </div>
   );
 };
